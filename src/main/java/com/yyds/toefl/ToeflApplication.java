9a4e1e9d1d4826750033ff3fd301365393811fb2
ql <254693270@qq.com>
2022-08-08 14:39:43 +0800

    init

---
+++ src/main/java/com/yyds/toefl/ToeflApplication.java
@@ -0,0 +1,13 @@
+package com.yyds.toefl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToeflApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToeflApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yyds/toefl/ToeflApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yyds.toefl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToeflApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
